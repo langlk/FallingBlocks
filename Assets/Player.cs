b6a403796c0ee9f244b886bb3ed4d33bd1fecdde
Kelsey Langlois <kels.langlois@gmail.com>
2021-12-15 11:11:40 -0800

    Add player object with movement.

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10;
+    float screenWrapPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenWrapPoint = Camera.main.aspect * Camera.main.orthographicSize + transform.localScale.x / 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        Vector3 velocity = input.normalized * speed;
+        transform.Translate(velocity * Time.deltaTime);
+
+        if (transform.position.x < -screenWrapPoint) {
+            transform.position = new Vector2(screenWrapPoint, transform.position.y);
+        } else if (transform.position.x > screenWrapPoint) {
+            transform.position = new Vector2(-screenWrapPoint, transform.position.y);
+        }
+    }
+}
