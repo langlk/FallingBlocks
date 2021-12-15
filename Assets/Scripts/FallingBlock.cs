099b2748c47ae3e3fee521a125b5ade0d0724f95
Kelsey Langlois <kels.langlois@gmail.com>
2021-12-15 12:48:31 -0800

    Destroy blocks after they fall below screen.

--- Assets/Scripts/FallingBlock.cs
+++ Assets/Scripts/FallingBlock.cs
@@ -6,14 +6,19 @@ public class FallingBlock : MonoBehaviour
 {
     public Vector2 speedRange = new Vector2(5, 13);
     float speed;
+    float screenBottom;
 
     void Start() {
         speed = Mathf.Lerp(speedRange.x, speedRange.y, Difficulty.GetDifficultyPercent());
+        screenBottom = -Camera.main.orthographicSize - transform.localScale.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.down * speed * Time.deltaTime, Space.Self);
+        if (transform.position.y < screenBottom) {
+            Destroy(gameObject);
+        }
     }
 }
